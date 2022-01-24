5fb8521b5156fc6723551ab69c918c9dfb5bbff4
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2022-01-24 13:29:46 +0100

    working

--- řady/řady.cpp
+++ řady/řady.cpp
@@ -5,6 +5,7 @@ int main(void) {
 	int minuly = 0, predminuly = 1, fibo;
 	for (int i = 0; i < 10; i++) {
 		fibo = minuly + predminuly;
+		//vypis cisel:
 		//printf("i: %d, predchozi: %d, predpredchozi: %d\n", i, minuly, predminuly);
 		printf("fibo(%d) = %d\n", i, fibo);
 		predminuly = minuly;
