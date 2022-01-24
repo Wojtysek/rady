a1d72119cefe5bb4c6cf6703868a35335073804f
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2022-01-24 13:24:07 +0100

    uprava

--- řady/řady.cpp
+++ řady/řady.cpp
@@ -1,5 +1,13 @@
 ﻿#include <stdio.h>
 
 int main(void) {
-
+	//generovani kraliku:
+	int minuly = 0, predminuly = 1, fibo;
+	for (int i = 0; i < 10; i++) {
+		fibo = minuly + predminuly;
+		//printf("i: %d, predchozi: %d, predpredchozi: %d\n", i, minuly, predminuly);
+		printf("fibo(%d) = %d\n", i, fibo);
+		predminuly = minuly;
+		minuly = fibo;
+	}
 }
